46329d93d62951276b23f4fc2cc0d50f7fdbdb14
Antoine Kaufmann <antoinek@cs.washington.edu>
2016-09-27 22:59:26 -0700

    tests: copy stage0 result to webserver

---
+++ .travis_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+track_status() {
+    while true ; do
+        date
+        sleep 30
+    done
+}
+
+
+mkdir ~/.ssh/
+mv .travis_id_rsa.pub ~/.ssh/id_rsa.pub
+mv .travis_id_rsa ~/.ssh/id_rsa
+
+make -C tools
+make -C tools setuid
+track_status &
+
+echo "Fetching stage0 files"
+cd distfiles && ./fetch.sh stage0 && cd ..
+echo "Testing Stage0 build"
+cd bootstrap/
+./stage0.sh || exit 1
+
+tar czf travis_stage0_prefix.tar.gz stage0_prefix
+scp stage0_prefix.tar.gz famkaufmann.info:public_html/
