e807726cac71c7f2c7f7d06273ea816fba6efdf2
Antoine Kaufmann <antoinek@cs.washington.edu>
2016-09-28 11:51:53 -0700

    tests: add stage1 test, fixed stage0 typo
    [test stage1]

--- .travis_test.sh
+++ .travis_test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 set -e
+set -x
 
 track_status() {
     while true ; do
@@ -20,6 +21,11 @@ make -C tools setuid
 track_status &
 
 if [ "$TEST_OPTION" = "stage0" ] ; then
+    git show | grep '\[test stage0\]' >/dev/null
+    if [ $? -ne 0 ] ; then
+        echo "Skipping stage 0 test"
+        exit 0
+    fi
     echo "Fetching stage0 files"
     cd distfiles && ./fetch.sh stage0 && cd ..
     echo "Testing Stage0 build"
@@ -29,6 +35,28 @@ if [ "$TEST_OPTION" = "stage0" ] ; then
     tar czf travis_stage0_prefix.tar.gz stage0_prefix
     scp travis_stage0_prefix.tar.gz \
         staticdistro@famkaufmann.info:public_html/travis_stage0_prefix.tar.gz.tmp
-    ssh staticditsro@famkaufmann.info \
+    ssh staticdistro@famkaufmann.info \
         "cd public_html && mv travis_stage0_prefix.tar.gz.tmp travis_stage0_prefix.tar.gz"
+elif [ "$TEST_OPTION" = "stage1" ] ; then
+    git show | grep '\[test stage1\]' >/dev/null
+    if [ $? -ne 0 ] ; then
+        echo "Skipping stage 1 test"
+        exit 0
+    fi
+    echo "Fetching stage1 files"
+    cd distfiles && ./fetch.sh stage1 && cd ..
+    echo "Testing Stage1 build"
+    cd bootstrap/
+
+    wget https://famkaufmann.info/~staticdistro/travis_stage0_prefix.tar.gz
+    tar xf travis_stage0_prefix.tar.gz
+
+    ./stage1.sh || exit 1
+
+    tar czf travis_stage1_packages.tar.gz stage1_packages
+    scp travis_stage1_packages.tar.gz \
+        staticdistro@famkaufmann.info:public_html/travis_stage1_packages.tar.gz.tmp
+    ssh staticdistro@famkaufmann.info \
+        "cd public_html && mv travis_stage1_packages.tar.gz.tmp travis_stage1_packages.tar.gz"
+
 fi
