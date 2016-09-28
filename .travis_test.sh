e81c1ad1a7afa9016d373cde59105230085d7311
Antoine Kaufmann <antoinek@cs.washington.edu>
2016-09-27 23:09:08 -0700

    tests: fix permissions

--- .travis_test.sh
+++ .travis_test.sh
@@ -24,5 +24,8 @@ if [ "$TEST_OPTION" == "stage0" ] ; then
     ./stage0.sh || exit 1
 
     tar czf travis_stage0_prefix.tar.gz stage0_prefix
-    scp stage0_prefix.tar.gz famkaufmann.info:public_html/
+    scp travis_stage0_prefix.tar.gz \
+        staticdistro@famkaufmann.info:public_html/travis_stage0_prefix.tar.gz.tmp
+    ssh staticditsro@famkaufmann.info \
+        "cd public_html && mv travis_stage0_prefix.tar.gz.tmp travis_stage0_prefix.tar.gz"
 fi
