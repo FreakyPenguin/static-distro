42c1560cc8d04704f9bca2ac42ee27dcf6d8989b
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-12 14:06:06 -0700

    packages/tar: set FORCE_UNSAFE_CONFIG to work as root
    This is necessary with user namespace now.

--- packages/tar/1.29/build.sh
+++ packages/tar/1.29/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 mkdir build && cd build
-../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
+FORCE_UNSAFE_CONFIGURE=1 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
     --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
     --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
     --disable-shared LDFLAGS=-static
