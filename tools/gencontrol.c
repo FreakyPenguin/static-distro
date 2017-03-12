0b6477fb55ea15f5fe1b5f411f6af290d78e9a6c
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-12 16:47:56 -0700

    gencontrol: add -o parameter to write output to file

--- tools/gencontrol.c
+++ tools/gencontrol.c
@@ -18,6 +18,7 @@ static char *file_name = NULL;
 static char *package_name = NULL;
 static enum version_mode ver_mode = VERSION_KEEP;
 static char *ver_param = NULL;
+static FILE *outf = NULL;
 
 int main(int argc, char *argv[])
 {
@@ -26,12 +27,14 @@ int main(int argc, char *argv[])
     struct source_control_bin *sb;
     struct control_dependency *cd;
 
+    outf = stdout;
     if (parse_params(argc, argv) != 0) {
         fprintf(stderr, "Usage: gencontrol [OPTIONS] CONTROL-FILE "
             "PACKAGE-NAME\n");
         fprintf(stderr, "Options:\n");
         fprintf(stderr, "    -v V: Override version number with V\n");
         fprintf(stderr, "    -V V: Append V to version number\n");
+        fprintf(stderr, "    -o F: Write control file to F\n");
         return EXIT_FAILURE;
     }
 
@@ -55,27 +58,30 @@ int main(int argc, char *argv[])
         return EXIT_FAILURE;
     }
 
-    printf("Package: %s\n", sb->package);
+    fprintf(outf, "Package: %s\n", sb->package);
 
     switch (ver_mode) {
         case VERSION_KEEP:
-            printf("Version: %s\n", c->version);
+            fprintf(outf, "Version: %s\n", c->version);
             break;
         case VERSION_OVERRIDE:
-            printf("Version: %s\n", ver_param);
+            fprintf(outf, "Version: %s\n", ver_param);
             break;
         case VERSION_APPEND:
-            printf("Version: %s%s\n", c->version, ver_param);
+            fprintf(outf, "Version: %s%s\n", c->version, ver_param);
             break;
         default:
             abort();
     }
 
-    printf("Depends-Run:\n");
+    fprintf(outf, "Depends-Run:\n");
     for (cd = sb->run_depend; cd != NULL; cd = cd->next)
-        printf("  %s\n", cd->package);
+        fprintf(outf, "  %s\n", cd->package);
 
     source_control_destroy(c);
+
+    if (outf != stdout)
+      fclose(outf);
 
     return EXIT_SUCCESS;
 }
@@ -83,7 +89,7 @@ int main(int argc, char *argv[])
 static int parse_params(int argc, char *argv[])
 {
     int c;
-    while ((c = getopt(argc, argv, "v:V:")) != -1) {
+    while ((c = getopt(argc, argv, "v:V:o:")) != -1) {
         switch (c) {
             case 'v':
                 ver_mode = VERSION_OVERRIDE;
@@ -92,6 +98,12 @@ static int parse_params(int argc, char *argv[])
             case 'V':
                 ver_mode = VERSION_APPEND;
                 ver_param = optarg;
+                break;
+            case 'o':
+                if ((outf = fopen(optarg, "w")) == NULL) {
+                    fprintf(stderr, "Opening output file failed: %s\n", optarg);
+                    return -1;
+                }
                 break;
             case '?':
                 fprintf(stderr, "Unknown option: %c\n", optopt);
