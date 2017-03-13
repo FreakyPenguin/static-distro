1eee03505c67937a48ce267af2ea043b1192a2bf
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-13 10:01:46 -0700

    tools: add -all-static to package ldflags
    [test stage1]

--- tools/build.sh
+++ tools/build.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 make clean
-make CC=${ARCH_TARGET}-gcc PREFIX="${PKG_DIR}" DESTDIR="${PKG_INSTDIR}" install
+make CC=${ARCH_TARGET}-gcc PREFIX="${PKG_DIR}" DESTDIR="${PKG_INSTDIR}" \
+  LDFLAGS=-static install
