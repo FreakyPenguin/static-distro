0a62a137424b5c5344d6d2daec0e334e4b562f14
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-13 15:25:38 -0700

    bootstrap: split stage2 into stage3
    stage2 now only contains packages needed to both unpack and build stage3
    packages in isolated environments (patch, tar, gzip,...)

--- bootstrap/stage3.sh
+++ bootstrap/stage3.sh
@@ -2,21 +2,21 @@
 #set -x
 set -e
 
-s1_pkgs="`pwd`/stage1_packages"
+s2_pkgs="`pwd`/stage2_packages"
 distfiles="`pwd`/../distfiles"
 packages="`pwd`/../packages"
 toolsdir="`pwd`/../tools"
-outdir="`pwd`/stage2_packages"
-buildparentdir="`pwd`/stage2_build"
+outdir="`pwd`/stage3_packages"
+buildparentdir="`pwd`/stage3_build"
 
 export ARCH=x86_64-linux-musl
 export ARCH_BUILD=$ARCH
 export ARCH_TARGET=$ARCH
 export PATH="$toolsdir:$PATH"
 
-# create stage2_packages folder, copy stage1 packages
+# create stage3_packages folder, copy stage2 packages
 if [ ! -d "$outdir" ] ; then
-    cp -r $s1_pkgs $outdir
+    cp -r $s2_pkgs $outdir
 fi
 
 # prepare directories
@@ -24,7 +24,7 @@ mkdir -p $buildparentdir
 
 failed() {
     echo "---------------------------------------------------------------------"
-    echo "$1 $(pwd) failed"
+    echo "$1 $build_dir failed"
     head -n 2000 "$build_dir/build.log"
     echo "---------------------------------------------------------------------"
     tail -n 2000 "$build_dir/build.log"
@@ -32,7 +32,7 @@ failed() {
     exit 1
 }
 
-build_s2_pkg() {
+build_s3_pkg() {
     pkg="$1"
     build_dir="${buildparentdir}/build-${pkg}"
     out_dir="${build_dir}/root"
@@ -44,31 +44,28 @@ build_s2_pkg() {
     control="$phys_path/control"
     #
     # make sure it's not completed yet
-    if [ -d "${outdir}/${pkg}/${ver}~~stage2" ] ; then
-        echo "stage2: Skipping $pkg/${ver}"
+    if [ -d "${outdir}/${pkg}/${ver}~~stage3" ] ; then
+        echo "stage3: Skipping $pkg/${ver}"
         return
     fi
 
-    echo "stage2: Building $pkg $ver"
+    echo "stage3: Building $pkg $ver"
 
     # prepare build directory
     rm -rf "$build_dir"
     cp -r "$phys_path" "$build_dir"
     mkdir -p "$out_dir"
 
-    # Unpack package
-    pkgbuild -B -C -w "$build_dir" -d "$distfiles" -V '~~stage2' "$control" \
-      >"$build_dir/build.log" 2>&1 || failed unpack
-
     # Build package
-    pkgbuild -U -p "$outdir" -w "$build_dir" -o root -V '~~stage2' "$control" \
-      >>"$build_dir/build.log" 2>&1 || failed build
+    pkgbuild -p "$outdir" -w "$build_dir" -d "$distfiles" -o root -V '~~stage3'\
+      "$control" >>"$build_dir/build.log" 2>&1 || failed build
 
     cp -r "${build_dir}/root/packages/${pkg}" "$outdir/"
     cd "${buildparentdir}"
 }
 
-stage2_packages="tar gzip bzip2 lzip xz"
-for p in $stage2_packages ; do
-    build_s2_pkg $p
+stage3_packages="binutils m4 gmp mpfr mpc musl-dynamic perl texinfo bison flex \
+    patch gcc"
+for p in $stage3_packages ; do
+    build_s3_pkg $p
 done
