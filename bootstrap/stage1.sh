e3f905a28765ecf0a5727419a885ff30af3396fd
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-13 09:23:49 -0700

    bootstrap/stage1: build package for tools
    [test stage1]

--- bootstrap/stage1.sh
+++ bootstrap/stage1.sh
@@ -57,7 +57,38 @@ build_s1_pkg() {
     cd "${buildparentdir}"
 }
 
+build_sdtools_pkg() {
+    pkg="staticdistro-tools"
+    build_dir="${buildparentdir}/build-${pkg}"
+    root_dir="${build_dir}/root"
+
+    # make sure it's not completed yet
+    if [ -d "${outdir}/${pkg}" ] ; then
+        echo "stage1: Skipping $pkg"
+        return
+    fi
+
+    # figure out version
+    phys_path="`cd ${toolsdir} && pwd -P`"
+    control="$phys_path/control"
+    ver="0.0.`date +'%Y%m%d%H%M'`~~stage1"
+
+    echo "stage1: Building $pkg $ver"
+
+    # prepare build directory
+    rm -rf "$build_dir"
+    cp -r "$phys_path" "$build_dir"
+    mkdir -p "$root_dir"
+
+    pkgbuild -w "$build_dir" -o "$root_dir" -d "$distfiles" -v "$ver" \
+      "$control" >"$build_dir/build.log" 2>&1 || failed build
+
+    cp -r "${build_dir}/root/packages/${pkg}" "$outdir/"
+    cd "${buildparentdir}"
+}
+
 stage1_packages="binutils gcc musl sbase mksh make sed grep awk"
 for p in $stage1_packages ; do
     build_s1_pkg $p
 done
+build_sdtools_pkg
