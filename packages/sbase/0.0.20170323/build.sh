2722b4af48cc1a9af939a10ad46796a98da67d61
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 14:44:50 -0700

    packages: sbase split out sbase-{doc,strings,grep,awk,sed,tar}

--- packages/sbase/0.0.20170323/build.sh
+++ packages/sbase/0.0.20170323/build.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 cd ${PKG_NAME}-*/
 
 case $PKG_VERSION in
     *stage1)
-        make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${PKG_DIR}/man" \
+        make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${docprefix}/man" \
             CC=${ARCH_TARGET}-gcc AR=${ARCH_TARGET}-ar RALIB=${ARCH_TARGET}-ranlib \
             LDFLAGS="-s -static" DESTDIR="${PKG_INSTDIR}"
         ;;
     *)
-        make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${PKG_DIR}/man" \
+        make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${docprefix}/man" \
             CC=gcc LDFLAGS="-s -static" DESTDIR="${PKG_INSTDIR}"
         ;;
 esac
-# remove some binaries
+
+# Split out separate packages for these
 for b in strings grep sed tar ; do
-    rm "${PKG_INSTDIR}/${PKG_DIR}/bin/$b"
+    dir="${PKG_INSTDIR}/packages/${PKG_NAME}-${b}/${PKG_VERSION}/bin"
+    mkdir -p "$dir"
+    mv "${PKG_INSTDIR}/${PKG_DIR}/bin/$b" "$dir/"
 done
