60fbf8162d8a0f125816b361ee0fe35a6ef9deff
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 15:09:08 -0700

    packages: gcc split out gcc-doc

--- packages/gcc/6.3.0/build.sh
+++ packages/gcc/6.3.0/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 mkdir build && cd build
 
 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${PKG_DIR}/include --localedir=${PKG_DIR}/locale \
+    --infodir=${docprefix}/info --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --build=$ARCH_BUILD --target=$ARCH_TARGET --host=$ARCH_TARGET \
     --enable-languages=c,c++ --disable-nls --disable-libmudflap \
     --disable-libsanitizer --disable-werror --disable-multilib \
@@ -16,4 +18,5 @@ mkdir build && cd build
 
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
+rm -f ${PKG_INSTDIR}/${docprefix}/info/dir \
+    ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
