454b9a8c1deb34df287c6b76de056d8b05adbf4d
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 12:28:47 -0700

    packages: xz split out xz-dev

--- packages/xz/5.2.2/build.sh
+++ packages/xz/5.2.2/build.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
+devprefix=/packages/${PKG_NAME}-dev/${PKG_VERSION}
 mkdir build && cd build
 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${PKG_DIR}/include --infodir=${docprefix}/info \
+    --localedir=${PKG_DIR}/locale --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --disable-shared LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
+
+# prepare dev-package
+mkdir -p ${PKG_INSTDIR}/${devprefix}
+mv ${PKG_INSTDIR}/${PKG_DIR}/lib ${PKG_INSTDIR}/${PKG_DIR}/include \
+    ${PKG_INSTDIR}/${devprefix}/
