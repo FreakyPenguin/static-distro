03b5904821a7fa9140053b6e51df7e7f417b7600
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 12:22:01 -0700

    packages: bzip2 split out bzip2-dev bzip2-doc

--- packages/bzip2/1.0.6/build.sh
+++ packages/bzip2/1.0.6/build.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
+devprefix=/packages/${PKG_NAME}-dev/${PKG_VERSION}
 cd ${PKG_NAME}-*/
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}" PREFIX="${PKG_DIR}"
+
+# prepare doc package
+mkdir -p ${PKG_INSTDIR}/${docprefix}
+mv ${PKG_INSTDIR}/${PKG_DIR}/man ${PKG_INSTDIR}/${docprefix}/
+
+# prepare dev package
+mkdir -p ${PKG_INSTDIR}/${devprefix}
+mv ${PKG_INSTDIR}/${PKG_DIR}/lib ${PKG_INSTDIR}/${PKG_DIR}/include \
+    ${PKG_INSTDIR}/${devprefix}/
