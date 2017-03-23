5fdbf5950b0fffdcbbe1dab9a0f5a3602ef499fa
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 14:53:56 -0700

    packages: make split out make-doc

--- packages/make/4.2.1/build.sh
+++ packages/make/4.2.1/build.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 mkdir build && cd build
 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${PKG_DIR}/include --localedir=${PKG_DIR}/locale \
+    --infodir=${docprefix}/info --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --build=$ARCH_BUILD --target=$ARCH_TARGET --host=$ARCH_TARGET \
     --disable-shared --disable-load LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
+rm -rf ${PKG_INSTDIR}/${docprefix}/info/dir \
+    ${PKG_INSTDIR}/${PKG_DIR}/include
