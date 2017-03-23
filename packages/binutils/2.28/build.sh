2281f3812bace78f1b51baed179b22eb018afea0
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 14:34:47 -0700

    packages: binutils split out binutils-doc

--- packages/binutils/2.28/build.sh
+++ packages/binutils/2.28/build.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
+devprefix=/packages/${PKG_NAME}-dev/${PKG_VERSION}
 mkdir build && cd build
 
 case $PKG_VERSION in
     *stage1)
         ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-            --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-            --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man \
-            --docdir=${PKG_DIR}/doc \
+            --oldincludedir=${devprefix}/include --libdir=${devprefix}/lib \
+            --includedir=${devprefix}/include \
+            --localedir=${PKG_DIR}/locale --mandir=${docprefix}/man \
+            --docdir=${docprefix}/doc --infodir=${docprefix}/info \
             --build=$ARCH_BUILD --target=$ARCH_TARGET --host=$ARCH_TARGET \
             --disable-werror --disable-nls --disable-gdb \
             --disable-libdecnumber --disable-readline --disable-sim
@@ -18,12 +21,15 @@ case $PKG_VERSION in
 
     *)
         ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-            --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-            --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man \
-            --docdir=${PKG_DIR}/doc --disable-werror --disable-shared \
+            --oldincludedir=${devprefix}/include --libdir=${devprefix}/lib \
+            --includedir=${devprefix}/include \
+            --localedir=${PKG_DIR}/locale --mandir=${docprefix}/man \
+            --docdir=${docprefix}/doc --infodir=${docprefix}/info \
+            --disable-werror --disable-shared \
             --disable-nls LDFLAGS=-static
         make $MAKE_JOBS
         make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
         ;;
 esac
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
+rm -f ${PKG_INSTDIR}/${docprefix}/info/dir \
+    ${PKG_INSTDIR}/${devprefix}/lib/charset.alias
