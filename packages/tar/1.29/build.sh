4c459e1f09654459c37ab9c5827e2777c99957f5
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 12:23:32 -0700

    packages: tar split out tar-doc

--- packages/tar/1.29/build.sh
+++ packages/tar/1.29/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
+docinst=${PKG_INSTDIR}/${docprefix}
 mkdir build && cd build
 FORCE_UNSAFE_CONFIGURE=1 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${PKG_DIR}/include --infodir=${docprefix}/info \
+    --localedir=${PKG_DIR}/locale --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --disable-shared LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
+rm -rf ${docinst}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/
