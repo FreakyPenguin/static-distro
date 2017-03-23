f927edcbbdba4502cca187591aee0a40c3962507
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 14:20:40 -0700

    packages: make ensure binary is acutually static

--- packages/make/4.2.1/build.sh
+++ packages/make/4.2.1/build.sh
@@ -5,7 +5,7 @@ mkdir build && cd build
     --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
     --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
     --build=$ARCH_BUILD --target=$ARCH_TARGET --host=$ARCH_TARGET \
-    --disable-shared LDFLAGS=-static
+    --disable-shared --disable-load LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
 rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
