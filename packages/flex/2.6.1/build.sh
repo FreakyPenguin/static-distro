61bc634c30bc32a014fb683b603de2ae0d418a74
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 20:05:19 -0700

    packages: flex split out flex-dev, flex-doc

--- packages/flex/2.6.1/build.sh
+++ packages/flex/2.6.1/build.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 set -e
+devprefix=/packages/${PKG_NAME}-dev/${PKG_VERSION}
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 mkdir build && cd build
 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${devprefix}/include --includedir=${devprefix}/include \
+    --libdir=${devprefix}/lib --localedir=${PKG_DIR}/locale \
+    --infodir=${docprefix}/info --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --disable-werror --disable-shared LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir ${PKG_INSTDIR}/${PKG_DIR}/lib/charset.alias
+rm -f ${PKG_INSTDIR}/${docprefix}/info/dir \
+    ${PKG_INSTDIR}/${devprefix}/lib/charset.alias
