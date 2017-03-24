a4dc08bca5e0c440337c5302dc26f224cdab5e71
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-03-23 20:10:33 -0700

    packages: texinfo split out texinfo-doc

--- packages/texinfo/6.1/build.sh
+++ packages/texinfo/6.1/build.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 mkdir build && cd build
 ../${PKG_NAME}-*/configure --prefix="${PKG_DIR}" \
-    --oldincludedir=${PKG_DIR}/include --infodir=${PKG_DIR}/info \
-    --localedir=${PKG_DIR}/locale --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc \
+    --oldincludedir=${PKG_DIR}/include --localedir=${PKG_DIR}/locale \
+    --infodir=${docprefix}/info --mandir=${docprefix}/man \
+    --docdir=${docprefix}/doc \
     --disable-werror --disable-shared LDFLAGS=-static
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
-rm -f ${PKG_INSTDIR}/${PKG_DIR}/info/dir
+rm -f ${PKG_INSTDIR}/${docprefix}/info/dir
