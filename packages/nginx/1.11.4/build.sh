4de5b13be74099ecb9708ea43a467f8b9f85e930
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-04-02 12:19:56 -0700

    packages/nginx: enable ipv6 and ssl module

--- packages/nginx/1.11.4/build.sh
+++ packages/nginx/1.11.4/build.sh
@@ -2,6 +2,7 @@
 set -e
 cd ${PKG_NAME}-*/
 ./configure --prefix="${PKG_DIR}" --sbin-path="${PKG_DIR}/bin/nginx" \
-    --with-ld-opt="-static -s" --with-cc=gcc
+    --with-ld-opt="-static -s" --with-cc=gcc --with-http_ssl_module \
+    --with-ipv6
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
