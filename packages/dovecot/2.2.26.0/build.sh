f671a539670e2cce1affdd3fb4fc3c790b4530c3
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-04-06 18:12:44 +0000

    packages/dovecot: split out dovecot-dev and dovecot-doc

--- packages/dovecot/2.2.26.0/build.sh
+++ packages/dovecot/2.2.26.0/build.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
+devprefix=/packages/${PKG_NAME}-dev/${PKG_VERSION}
 #mkdir build && cd build
 cd ${PKG_NAME}-*/
 ./configure --prefix="${PKG_DIR}" \
-    --infodir=${PKG_DIR}/info --localedir=${PKG_DIR}/locale \
-    --mandir=${PKG_DIR}/man --docdir=${PKG_DIR}/doc --sbindir=${PKG_DIR}/bin \
+    --infodir=${PKG_DIR}/info --localedir=${PKG_DIR}/locale --sbindir=${PKG_DIR}/bin \
+    --mandir=${docprefix}/man --docdir=${docprefix}/doc  --sysconfdir=${docprefix}/etc \
+    --libdir=${devprefix}/lib --includedir=${devprefix}/include \
     --enable-static --disable-shared LDFLAGS="-static -s"
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
