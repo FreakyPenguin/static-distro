b3462e3ead4fe2999fa5cb68ea74bb7bd066127a
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-04-06 18:11:33 +0000

    packages/ubase: split out ubase-doc

--- packages/ubase/0.0.20160925/build.sh
+++ packages/ubase/0.0.20160925/build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
+docprefix=/packages/${PKG_NAME}-doc/${PKG_VERSION}
 cd ${PKG_NAME}-*/
-make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${PKG_DIR}/man" \
+make $MAKE_JOBS all install PREFIX="${PKG_DIR}" MANPREFIX="${docprefix}/man" \
     CC=gcc LDFLAGS="-s -static" DESTDIR="${PKG_INSTDIR}"
