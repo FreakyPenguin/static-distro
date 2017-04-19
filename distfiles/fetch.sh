5e319d749e958262aa4ec884cf1d00e63c40f52d
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-04-19 12:14:21 -0700

    packages/libev: added

--- distfiles/fetch.sh
+++ distfiles/fetch.sh
@@ -76,6 +76,7 @@ ${GET} "http://sv0.famkaufmann.info/~staticdistro/distfiles/libuv-v1.9.1.tar.gz"
 ${GET} "http://www.cmake.org/files/v3.7/cmake-3.7.2.tar.gz"
 ${GET} "http://xmlsoft.org/sources/libxml2-2.9.4.tar.gz"
 ${GET} "https://kristaps.bsd.lv/acme-client/snapshots/acme-client-portable-0.1.16.tgz"
+${GET} "http://dist.schmorp.de/libev/libev-4.24.tar.gz"
 
 # old versions
 ${GET} "https://ftp.gnu.org/gnu/binutils/binutils-2.26.1.tar.gz"
