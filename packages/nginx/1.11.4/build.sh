a7c527d38890d1473301013dcf4a648a3d89de51
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-09-10 17:11:38 +0000

    packages/nginx: include realip module

--- packages/nginx/1.11.4/build.sh
+++ packages/nginx/1.11.4/build.sh
@@ -3,6 +3,6 @@ set -e
 cd ${PKG_NAME}-*/
 ./configure --prefix="${PKG_DIR}" --sbin-path="${PKG_DIR}/bin/nginx" \
     --with-ld-opt="-static -s" --with-cc=gcc --with-http_ssl_module \
-    --with-ipv6
+    --with-http_realip_module --with-ipv6
 make $MAKE_JOBS
 make $MAKE_JOBS install DESTDIR="${PKG_INSTDIR}"
