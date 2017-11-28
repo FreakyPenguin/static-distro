9292bfd048dc733e5132d8898a04a0ecdbcd57f9
Antoine Kaufmann <antoinek@cs.washington.edu>
2017-11-28 12:50:23 -0800

    tools: pkgcontrol can dump out source field

--- tools/pkgcontrol.c
+++ tools/pkgcontrol.c
@@ -10,6 +10,7 @@ enum field {
     FIELD_UNSPECIFIED,
     FIELD_NAME,
     FIELD_VERSION,
+    FIELD_SOURCE,
     FIELD_DEP_RUN,
     FIELD_BUILT_FROM,
 };
@@ -28,6 +29,7 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Commands:\n");
         fprintf(stderr, "    -n: Dump package name\n");
         fprintf(stderr, "    -v: Dump package version\n");
+        fprintf(stderr, "    -s: Dump package source\n");
         fprintf(stderr, "    -r: Dump package runtime dependencies\n");
         fprintf(stderr, "    -b: Dump package built froms\n");
         return EXIT_FAILURE;
@@ -45,6 +47,9 @@ int main(int argc, char *argv[])
                 break;
             case FIELD_VERSION:
                 puts(c->version);
+                break;
+            case FIELD_SOURCE:
+                printf("%s,%s\n", c->src_package, c->src_version);
                 break;
             case FIELD_DEP_RUN:
                 for (cd = c->run_depend; cd != NULL; cd = cd->next) {
@@ -68,13 +73,16 @@ int main(int argc, char *argv[])
 static int parse_params(int argc, char *argv[])
 {
     int c;
-    while ((c = getopt(argc, argv, "nvrb")) != -1) {
+    while ((c = getopt(argc, argv, "nvsrb")) != -1) {
         switch (c) {
             case 'n':
                 field = FIELD_NAME;
                 break;
             case 'v':
                 field = FIELD_VERSION;
+                break;
+            case 's':
+                field = FIELD_SOURCE;
                 break;
             case 'r':
                 field = FIELD_DEP_RUN;
